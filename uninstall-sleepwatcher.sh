703fe861635d58c66faf5598c56deec6873f0e1a
nanguo.sjh <nanguo.sjh@alibaba-inc.com>
2019-01-15 09:54:02 +0800

    uninstall updated

---
+++ uninstall-sleepwatcher.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Install sleepwatcher
+sudo rm /usr/local/sbin/sleepwatcher
+sudo rm /usr/local/share/man/man8/sleepwatcher.8
+sudo rm /Library/LaunchAgents/de.bernhard-baehr.sleepwatcher-20compatibility.plist
+sudo rm /etc/rc.sleep
+sudo rm /etc/rc.wakeup
+
